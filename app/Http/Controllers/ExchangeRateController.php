628d6ec9a76c457fa6a2f4ffe24f866419a77ff4
mashuk-dev <mushfiqur@aapbd.com>
2022-01-24 02:17:37 +0600

    Adjusted api call according to free account limitations

--- app/Http/Controllers/ExchangeRateController.php
+++ app/Http/Controllers/ExchangeRateController.php
@@ -10,9 +10,15 @@ class ExchangeRateController extends Controller
     public function index(Request $request){
         $current_user_currency = $request->currentUserCurrency;
         $other_user_currency = $request->otherUserCurrency;
-        $param = $current_user_currency.$other_user_currency;
-        $response = Http::get('http://api.currencylayer.com/live?access_key=23fabde029d84fdcfd0d3c311cdc78a2&source='.$current_user_currency.'&currencies='. $other_user_currency.'&format=1');
+        $response = Http::get('http://api.currencylayer.com/live?access_key=23fabde029d84fdcfd0d3c311cdc78a2&format=1');
         $result = $response->json();
-        return $result['quotes'][$param];
+        if($current_user_currency == 'USD'){
+            $param = $current_user_currency.$other_user_currency;
+            return $result['quotes'][$param];
+        }else{
+            //free api not support changing base currency //currencylayer.com
+            $param = "USDEUR";
+            return (1/$result['quotes'][$param]);
+        }
     }
 }
